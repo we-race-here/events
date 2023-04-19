c2ae7609e92a51c14f251bbbd19dc274f89f7a92
Vincent Davis <vincent@heteroskedastic.net>
2023-04-19 13:39:02 -0600

    updates to install

--- install_configure/install.sh
+++ install_configure/install.sh
@@ -1,7 +1,13 @@
-For new installs, run this script to install and configure the server.
-App will run on port 9018
-apt install docker -y
-apt install docker-compose -y
+#For new installs, run this script to install and configure the server.
+#App will run on port 9018
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+
 mkdir /opt/webapps/
 cd /opt/webapps/
 git clone https://github.com/we-race-here/events.git -b main
