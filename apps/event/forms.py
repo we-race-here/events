4a4f0e83f82d348d7009b0218fa2c79309e28dd9
Vincent Davis <vincent@heteroskedastic.net>
2023-04-25 12:13:17 -0600

    added additional logic to create event form

--- apps/event/forms.py
+++ apps/event/forms.py
@@ -1,11 +1,19 @@
 from django import forms
+from django.forms import Textarea
 from apps.event.models import Event
 from django.forms import DateField, DateInput
 
 
 class EventForm(forms.ModelForm):
-    start_date = DateField(required=True, label="Date of Birth", widget=DateInput(attrs={'type': 'date', 'class': "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md  rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"}))
-
+    start_date = DateField(required=True, label="Start Date", widget=DateInput(attrs={'type': 'date', 'class': "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md  rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"}))
+    end_date = DateField(required=True, label="End Date", widget=DateInput(attrs={'type': 'date', 'class': "appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md  rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"}))
     class Meta:
         model = Event
-        fields = ['name', 'description', 'start_date']
+        fields = ['name', 'featured_event', 'blurb', 'description', 'start_date', 'end_date', 'email', 'website', 'registration_website',
+                  'permit_no', 'is_usac_permitted', 'city', 'state', 'country', 'tags', 'panels', 'organization', 'publish_type', 'approved']
+        widgets = {
+            "name": Textarea(attrs={"cols": 80, "rows": 20}),
+            "blurb": Textarea(attrs={"cols": 80, "rows": 100}),
+            "description": Textarea(attrs={"cols": 80, "rows": 200}),
+        }
+
