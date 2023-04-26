afd1f115d86408f759a59fc4454c15aa23da4e72
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2023-04-26 20:50:57 +0530

    Added search and filter for org list view

--- apps/membership/views.py
+++ apps/membership/views.py
@@ -6,6 +6,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.detail import DetailView
 from .models import Organization
 from .forms import OrganizationForm
+from django.db.models import Q
 
 class CreateOrganizationView(CreateView):
     model = Organization
@@ -31,3 +32,27 @@ class OrganizationListView(ListView):
     template_name = 'org/organization_list.html'
     context_object_name = 'organizations'
     paginate_by = 10  # Change this to the desired number of items per page
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['type_choices'] = Organization.TYPE_CHOICES
+        return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        search_query = self.request.GET.get('search', '')
+        type_filter = self.request.GET.get('type', '')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(website__icontains=search_query) |
+                Q(city__icontains=search_query) |
+                Q(state__icontains=search_query)
+            )
+
+        if type_filter:
+            queryset = queryset.filter(type=type_filter)
+
+        return queryset
