ca33cfb79aafbed33af1e898a876e965c096182a
Karthick Sakkaravarthi <37908485+karthicksakkaravarti@users.noreply.github.com>
2023-04-29 22:31:08 +0530

    Added membership links

--- apps/membership/urls.py
+++ apps/membership/urls.py
@@ -1,6 +1,7 @@
 # urls.py
 from django.urls import path
 from .views import OrganizationListView, CreateOrganizationView, OrganizationDetailView, UpdateOrganizationView, DeleteOrganizationView
+app_name = 'membership'
 
 urlpatterns = [
     path('org', OrganizationListView.as_view(), name='organizations'),
