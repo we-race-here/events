9b196d7f659d931162c964c71721eac16a0ac220
Vincent Davis <vincent@heteroskedastic.net>
2023-04-29 07:28:14 -0600

    upgrade to python 4.2

--- config/settings/production.py
+++ config/settings/production.py
@@ -50,7 +50,11 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool("DJANGO_SECURE_CONTENT_TYPE_NOSNIFF", def
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 # MEDIA
 # ------------------------------------------------------------------------------
 
