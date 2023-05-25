decb1238da1b3ca9c4683f03abad151c219127f2
Vincent Davis <vincent@heteroskedastic.net>
2023-05-25 11:11:58 -0600

    update settings add django toolbar to dev server

---
+++ config/settings/development.py
@@ -0,0 +1,27 @@
+from .base import *  # noqa
+from .production import *  # noqa
+from .production import MIDDLEWARE, env, INSTALLED_APPS
+
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ["debug_toolbar"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa: F405
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+# for adding remote ip address
+MY_IP = env("MY_IP", default=False)
+if MY_IP:
+    INTERNAL_IPS += [MY_IP]
+if env("USE_DOCKER", default=False) == "yes":
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
