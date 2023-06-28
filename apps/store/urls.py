d58ecfca63280d63af2f1f52e1d111c327c235b6
Vincent Davis <vincent@heteroskedastic.net>
2023-06-28 13:49:23 -0600

    add stripe to admin menu

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 
 # now import the views.py file into this code
 from . import views
+from .views import StripeAccounting
 
+app_name = "store"
 urlpatterns = [
     path("", views.index),
     path("store/webhook/payments/", views.payment_webhook, name="payment_webhook"),
+    path("store/stripe_accounting/", StripeAccounting.as_view(), name="stripe_accounting"),
 ]
