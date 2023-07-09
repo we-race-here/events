a3a554c5b0b0bab308810eec3ef0f13e987c1537
Vincent Davis <vincent@heteroskedastic.net>
2023-07-09 08:44:59 -0600

    Update ckeditor

--- apps/event/forms.py
+++ apps/event/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 from django.forms import CharField, DateField, DateInput, ModelChoiceField, ModelForm, TimeField, TimeInput
 
 from apps.membership.models import Organization
-
 from .models import Event, Race, RaceResult, RaceSeries, event_types
 
 User = get_user_model()
@@ -78,15 +77,11 @@ class EventForm(forms.ModelForm):
             }
         ),
     )
-    description = forms.CharField(widget=forms.Textarea(attrs={"class": "ckeditor"}))
+    description = forms.CharField(widget=forms.Textarea(attrs={"class": "editor-basic"}))
 
     class Meta:
         model = Event
-        css = (
-            "block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 "
-            "appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none "
-            "focus:ring-0 focus:border-blue-600 peer"
-        )
+        css = ()
         fields = [
             "name",
             "blurb",
--- events/static/js/project.js
+++ events/static/js/project.js
@@ -1,1 +1,19 @@
 /* Project specific Javascript goes here. */
+
+// Basic Editor
+ClassicEditor
+        .create(document.querySelector('.editor-basic'), {
+            toolbar: ['bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote']
+        })
+        .catch(error => {
+            console.error(error);
+        });
+
+// Full Editor
+ClassicEditor
+    .create(document.querySelector('.editor-full'), {
+        toolbar: ['bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote', 'imageUpload', 'insertTable', 'mediaEmbed']
+    })
+    .catch(error => {
+        console.error(error);
+    });
