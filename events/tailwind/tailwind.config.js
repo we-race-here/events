cfc0655d1f6b5952945ae74ba0962d944fe8ff41
Vincent Davis <vincent@heteroskedastic.net>
2023-07-12 09:46:19 -0600

    update tailwind config, updates to base

--- events/tailwind/tailwind.config.js
+++ events/tailwind/tailwind.config.js
@@ -1,21 +1,34 @@
+const colors = require('tailwindcss/colors')
 module.exports = {
     // purge: {
-        content: [
-            '../templates/**/*.html',
-            '../../templates/**/*.html',
-            '../../**/templates/**/*.html',
-            // '../../**/*.js',
-            // '../../**/*.py'
-        ],
-        // options: {
-        //     safelist: [], // Specify the classes that should not be removed by purge
-        // }
+    content: [
+        '../templates/**/*.html',
+        '../../templates/**/*.html',
+        '../../**/templates/**/*.html',
+        // '../../**/*.js',
+        '../users/forms.py',
+        '../../apps/event/forms.py',
+        '../../apps/membership/forms.py',
+        '../../apps/store/forms.py',
+        '../../apps/usac/forms.py',
+    ],
+    // options: {
+    //     safelist: [], // Specify the classes that should not be removed by purge
+    // }
     // },
     theme: {
         extend: {
             fontFamily: {
                 'sans': ['YourFont', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
             },
+            colors: {
+                'bc_cyan': colors.cyan[600],
+                'bc_cyan_hover': colors.cyan[800],
+                'bc_orange': colors.orange[600],
+                'bc_orange_hover': colors.orange[800],
+                'bc_green': colors.green[600],
+                'bc_green_hover': colors.green[800],
+            }
         },
     },
     plugins: [
