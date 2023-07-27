c61b191a7a1ba26992772b014dee9e88399f02b8
Vincent Davis <vincent@heteroskedastic.net>
2023-07-27 08:01:43 -0600

    Community submit event form

---
+++ events/utils/events_utils.py
@@ -0,0 +1,24 @@
+from django.conf import settings
+from django.core.mail import send_mail
+
+
+def sys_send_mail(subject, message, from_email=None, recipient_list=[], recipient_system=[], fail_silently=False,
+                  html_message=None):
+    # send to user
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email or settings.DEFAULT_FROM_EMAIL,
+        recipient_list=recipient_list,
+        html_message=html_message,
+        fail_silently=fail_silently,
+    )
+    # send to admins
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=from_email or settings.DEFAULT_FROM_EMAIL,
+        recipient_list=recipient_system + settings.NOISY_EMAILS,
+        html_message=html_message,
+        fail_silently=fail_silently,
+    )
